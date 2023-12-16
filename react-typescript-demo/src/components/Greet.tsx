a15af2772e7286b99ca1061c6f575b5132123347
brian-ambani <brainambani1@gmail.com>
2023-12-17 02:43:38 +0300

    destructed the props

--- react-typescript-demo/src/components/Greet.tsx
+++ react-typescript-demo/src/components/Greet.tsx
@@ -3,13 +3,12 @@ type GreetProps = {
   messageCount?: Number;
   isLoggedIn: Boolean;
 };
-export const Greet = (props: GreetProps) => {
-  const { messageCount = 0 } = props;
+export const Greet = ({ name, messageCount, isLoggedIn }: GreetProps) => {
   return (
     <div>
       <h2>
-        {props.isLoggedIn
-          ? `Welcome ${props.name}! You have ${messageCount} unread
+        {isLoggedIn
+          ? `Welcome ${name}! You have ${messageCount} unread
         messages`
           : "Welcome Guest!"}
       </h2>
