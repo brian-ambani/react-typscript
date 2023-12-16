65b35ee53cd96fea2a7c5701fd085e6cc362974b
brian-ambani <brainambani1@gmail.com>
2023-12-17 02:28:30 +0300

    updated the Status component with unions

--- react-typescript-demo/src/components/Status.tsx
+++ react-typescript-demo/src/components/Status.tsx
@@ -4,6 +4,7 @@ type StatusProps = {
 
 export const Status = (props: StatusProps) => {
   let message;
+
   if (props.status === "loading") {
     message = "Loading...";
   } else if (props.status === "success") {
